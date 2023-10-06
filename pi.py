357aad8bb00c29e9b759752e1be564b05a8f63ff
hinaWAKA <mh.perfect.insider@gmail.com>
2023-10-06 20:39:41 +0900

    Python入門の解答

--- pi.py
+++ pi.py
@@ -4,4 +4,13 @@ text = """
 """
 
 # TODO
+import re
 
+def pi_string_from_text(text):
+    # 単語の抽出（記号を無視）
+    words = re.findall(r'\b\w+\b', text)
+    # 各単語の文字数をカウントし、それを文字列に変換して連結
+    pi_string = "".join(map(str, map(len, words)))
+    return pi_string
+# 結果の出力
+print(pi_string_from_text(text))
--- quadratic_formula.py
+++ quadratic_formula.py
@@ -1,9 +1,20 @@
-a = 1
-b = -6
-c = 9
+import math
 
-# TODO
-x1 = ...
-x2 = ...
+def solve(a, b, c):
+    discriminant = b**2 - 4*a*c
+    if discriminant >= 0:
+        x1 = (-b + discriminant**0.5) / (2*a)
+        x2 = (-b - discriminant**0.5) / (2*a)
+    else:
+        # Handling complex roots
+        real_part = -b / (2*a)
+        imag_part = (abs(discriminant)**0.5) / (2*a)
+        x1 = complex(real_part, imag_part)
+        x2 = complex(real_part, -imag_part)
+    print(x1, x2)
 
-print(x1, x2)
+# Test the function with the provided equations
+solve(1, -6, 9)
+solve(1, 2, -8)
+solve(8, -6, -35)
+solve(1, 0, 1)
