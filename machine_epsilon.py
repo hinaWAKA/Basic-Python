dfb1655b932739cd1c8abed1df6c7cca6785968c
hinaWAKA <mh.perfect.insider@gmail.com>
2023-10-08 18:43:08 +0900

    制御構文の解答

--- machine_epsilon.py
+++ machine_epsilon.py
@@ -1,6 +1,13 @@
 # TODO
-#sys.float_info.epsilon という、浮動小数点数のマシンイプシロンを取得するための機能が sys モジュールに用意。
-import sys
-machine_epsilon = sys.float_info.epsilon
+
+epsilon = 1.0
+
+while 1.0 + epsilon > 1.0:
+    epsilon /= 2.0
+
+# epsilon は 1 と 1 + 2*epsilon の最小epsilon 
+# 求めるマシンイプシロンは 2*epsilon
+machine_epsilon = 2.0 * epsilon
 
 print(machine_epsilon)
+
